c061b2350c6ae53aeb221337d60a55fd2f8556ba
Steven Buss <steven.buss@gmail.com>
2013-12-08 22:50:10 -0800

    Better imports

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pytrade import order
+from pytrade.order import BUY, SELL, Order
 from tests.factories import OrderFactory
 
 
@@ -8,33 +8,42 @@ class TestOrderCreation(unittest.TestCase):
     def test_bad_type(self):
         attrs = OrderFactory.attributes()
         attrs['type'] = 'abc'
-        self.assertRaises(ValueError, order.Order, **attrs)
+        self.assertRaises(ValueError, Order, **attrs)
 
     def test_negative_amount(self):
         attrs = OrderFactory.attributes()
         attrs['amount'] = -100
-        self.assertRaises(ValueError, order.Order, **attrs)
+        self.assertRaises(ValueError, Order, **attrs)
 
     def test_negative_value(self):
         attrs = OrderFactory.attributes()
         attrs['value'] = -100
-        self.assertRaises(ValueError, order.Order, **attrs)
+        self.assertRaises(ValueError, Order, **attrs)
 
     def test_float_amount(self):
         attrs = OrderFactory.attributes()
         attrs['amount'] = 100.
-        self.assertRaises(ValueError, order.Order, **attrs)
+        self.assertRaises(ValueError, Order, **attrs)
 
     def test_float_value(self):
         attrs = OrderFactory.attributes()
         attrs['value'] = 100.
-        self.assertRaises(ValueError, order.Order, **attrs)
+        self.assertRaises(ValueError, Order, **attrs)
 
     def test_bad_user(self):
         attrs = OrderFactory.attributes()
         attrs['user'] = 'abc'
-        self.assertRaises(ValueError, order.Order, **attrs)
+        self.assertRaises(ValueError, Order, **attrs)
 
-    def test_good(self):
+    def test_good_buy(self):
         attrs = OrderFactory.attributes()
-        self.assertTrue(order.Order(**attrs))
+        order = Order(**attrs)
+        self.assertTrue(order)
+        self.assertEqual(order.type, BUY)
+
+    def test_good_sell(self):
+        attrs = OrderFactory.attributes()
+        attrs['type'] = SELL
+        order = Order(**attrs)
+        self.assertTrue(order)
+        self.assertEqual(order.type, SELL)
