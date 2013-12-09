8bddc6949a275a8f046df4c378fae7fb8e9dbea9
Steven Buss <steven.buss@gmail.com>
2013-12-08 23:14:01 -0800

    Add PriorityOrderBook tests and refactor

--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -1,7 +1,6 @@
 import unittest
 
 from pytrade.order import BUY
-from pytrade.order import Order
 from pytrade.order import SELL
 from pytrade.orderbook import ListOrderBook
 from pytrade.orderbook import OrderBook
@@ -19,28 +18,39 @@ class TestOrderBookCreation(unittest.TestCase):
         self.assertRaises(ValueError, book.add, order)
 
 
-class TestListOrderBook(unittest.TestCase):
-    def _get_order_book(self, type):
+class _BaseTestOrderBook(object):
+    """Subclasses must define cls.ORDER_BOOK_CLASS"""
+    ORDER_BOOK_CLASS = None
+
+    def get_order_book(self, type):
         orders = [OrderFactory(type=type, value=i) for i in range(1, 11)]
-        return orders, ListOrderBook(type, orders)
+        return orders, self.ORDER_BOOK_CLASS(type, orders)
 
     def test_buy_ordering(self):
-        orders, book = self._get_order_book(BUY)
+        orders, book = self.get_order_book(BUY)
         self.assertEqual(book.peek(), orders[-1])
         for order in orders[::-1]:
             self.assertEqual(order, book.pop())
 
     def test_sell_ordering(self):
-        orders, book = self._get_order_book(SELL)
+        orders, book = self.get_order_book(SELL)
         self.assertEqual(book.peek(), orders[0])
         for order in orders:
             self.assertEqual(order, book.pop())
 
     def test_cancel(self):
-        orders, book = self._get_order_book(SELL)
+        orders, book = self.get_order_book(SELL)
         self.assertEqual(book.peek(), orders[0])
         book.cancel(orders[0].id)
         self.assertEqual(book.peek(), orders[1])
         book.cancel(orders[2].id)
         self.assertEqual(book.pop(), orders[1])
         self.assertEqual(book.pop(), orders[3])
+
+
+class TestListOrderBook(_BaseTestOrderBook, unittest.TestCase):
+    ORDER_BOOK_CLASS = ListOrderBook
+
+
+class TestPriorityOrderBook(_BaseTestOrderBook, unittest.TestCase):
+    ORDER_BOOK_CLASS = PriorityOrderBook
