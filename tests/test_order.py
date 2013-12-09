73ee64498ef6a90b1531f012313836e12845a965
Steven Buss <steven.buss@gmail.com>
2013-12-08 22:48:04 -0800

    Expand tests

--- tests/test_order.py
+++ tests/test_order.py
@@ -8,4 +8,33 @@ class TestOrderCreation(unittest.TestCase):
     def test_bad_type(self):
         attrs = OrderFactory.attributes()
         attrs['type'] = 'abc'
-        self.assertRaises(ValueError, order.Order, *attrs)
+        self.assertRaises(ValueError, order.Order, **attrs)
+
+    def test_negative_amount(self):
+        attrs = OrderFactory.attributes()
+        attrs['amount'] = -100
+        self.assertRaises(ValueError, order.Order, **attrs)
+
+    def test_negative_value(self):
+        attrs = OrderFactory.attributes()
+        attrs['value'] = -100
+        self.assertRaises(ValueError, order.Order, **attrs)
+
+    def test_float_amount(self):
+        attrs = OrderFactory.attributes()
+        attrs['amount'] = 100.
+        self.assertRaises(ValueError, order.Order, **attrs)
+
+    def test_float_value(self):
+        attrs = OrderFactory.attributes()
+        attrs['value'] = 100.
+        self.assertRaises(ValueError, order.Order, **attrs)
+
+    def test_bad_user(self):
+        attrs = OrderFactory.attributes()
+        attrs['user'] = 'abc'
+        self.assertRaises(ValueError, order.Order, **attrs)
+
+    def test_good(self):
+        attrs = OrderFactory.attributes()
+        self.assertTrue(order.Order(**attrs))
