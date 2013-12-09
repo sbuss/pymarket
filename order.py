1f5960884dfdd48b06c867505f6e827831ff3505
Steven Buss <steven.buss@gmail.com>
2013-12-08 22:35:29 -0800

    Add Order and OrderBook

---
+++ order.py
@@ -0,0 +1,47 @@
+import uuid
+
+BUY = 'buy'
+SELL = 'sell'
+
+
+class Order(object):
+    def __init__(self, type, amount, value, user):
+        if type != BUY and type != SELL:
+            raise ValueError("type must be one of %s" % ([BUY, SELL],))
+        if amount < 1 or not isinstance(amount, int):
+            raise ValueError("Amount must be a positive integer")
+        if value < 1 or not isinstance(value, int):
+            raise ValueError("Value must be a positive integer")
+        if not isinstance(user, uuid.UUID):
+            raise ValueError("Users must be identified by their UUIDs")
+
+        self.id = uuid.uuid4()
+        self.type = type
+        self.amount = amount
+        self.value = value
+        self.user = user
+
+    def __lt__(self, other):
+        """
+        A BUY is less than another BUY if its value is GREATER.
+        A SELL is less than another SELL if its value is LESSER.
+
+        This weird construct is because we want to be able to efficiently
+        find the buys & sells that overlap in order to make the market,
+        which means matching the "smallest" buy with the smallest sell.
+
+            Buy@20 < Buy@10
+            Sell@20 < Sell@30
+
+        We can thus implement an OrderBook as a simple ordering of available
+        Buy and Sell orders and only have to look at the few smallest Orders,
+        which can typically be done quite efficiently.
+        """
+        if self.type == BUY:
+            return self.value > other.value
+        else:
+            return self.value < other.value
+
+    def __repr__(self):
+        return "Order<%s>(%s, %s, %s, %s)" % (
+            self.id, self.type, self.amount, self.value, self.user)
---
+++ orderbook.py
@@ -0,0 +1,105 @@
+import heapq
+
+from order import BUY, SELL
+
+
+class OrderBook(object):
+    def __init__(self, type, orders=None):
+        """Construct an OrderBook of type book_type.
+
+        This base class uses a naive sorted list to maintain the list of
+        orders.
+
+        Args:
+            type: Either order.BUY or order.SELL
+            orders: An optional list of Orders to start the book with.
+        """
+        if type != BUY and type != SELL:
+            raise ValueError("type must be one of %s" % ([BUY, SELL],))
+        self.type = type
+
+        if orders:
+            for order in orders:
+                self.add(order)
+
+    def _add(self, order):
+        raise NotImplementedError()
+
+    def _pop(self):
+        raise NotImplementedError()
+
+    def _peek(self):
+        raise NotImplementedError()
+
+    def _cancel(self, order):
+        raise NotImplementedError()
+
+    def add(self, order):
+        """Add an Order to the order book."""
+        if order.type != self.type:
+            raise ValueError("Order type (%s) does not match book type (%s)" %
+                             (order.type, self.type))
+        self._add(order)
+
+    def pop(self):
+        """Remove the top Order from the order book.
+
+        For Buy order books, the top Order is the one with the highest bid
+        price, for Sell order books, the top Order is the one with the lowest
+        offer price.
+        """
+        return self._pop()
+
+    def peek(self):
+        """View the current top Order in the order book."""
+        return self._peek()
+
+    def cancel(self, order_id):
+        """Cancel the given Order.
+
+        Args:
+            order_id: The ID of the Order to cancel. This does not take an
+                Order instance because it may have been partially filled and
+                wouldn't equal the Order passed in.
+
+        Returns True if the order was cancelled, False if it couldn't be
+        found.
+        """
+        return self._cancel(order_id)
+
+
+class ListOrderBook(OrderBook):
+    def __init__(self, *args, **kwargs):
+        self.orders = []
+        super(ListOrderBook, self).__init__(*args, **kwargs)
+
+    def _add(self, order):
+        self.orders.append(order)
+        self.orders = sorted(self.orders)
+
+    def _pop(self):
+        return self.orders.pop(0)
+
+    def _peek(self):
+        return self.orders[0]
+
+    def _cancel(self, order_id):
+        for i, order in enumerate(self.orders):
+            if order.id == order_id:
+                del self.orders[i]
+                return
+
+
+class PriorityOrderBook(ListOrderBook):
+    def _add(self, order):
+        heapq.heappush(self.orders, order)
+
+    def _pop(self):
+        return heapq.heappop(self.orders)
+
+    def _peek(self):
+        return self.orders[0]
+
+    def _cancel(self, order_id):
+        super(PriorityOrderBook, self)._cancel(order_id)
+        heapq.heapify(self.orders)
