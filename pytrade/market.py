baff34181925d7da9957946265afff33d275d15f
Steven Buss <steven.buss@gmail.com>
2013-12-10 00:05:02 -0800

    Use logger instead of print

--- pytrade/market.py
+++ pytrade/market.py
@@ -1,3 +1,10 @@
+import logging
+
+logger = logging.getLogger('pytrade.market')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+
+
 class Market(object):
     def __init__(self, buy_book, sell_book):
         self.buy_book = buy_book
@@ -9,20 +16,20 @@ class Market(object):
             top_buy = self.buy_book.peek()
             top_sell = self.sell_book.peek()
             if top_buy.value >= top_sell.value:
-                print("Top buy: %s" % top_buy)
-                print("Top sell: %s" % top_sell)
+                logger.info("Top buy: %s" % top_buy)
+                logger.info("Top sell: %s" % top_sell)
                 top_buy = self.buy_book.pop()
                 top_sell = self.sell_book.pop()
                 if top_buy.amount > top_sell.amount:
                     top_buy.amount -= top_sell.amount
                     self.buy_book.add(top_buy)
-                    print("Bought %s for $%s" %
-                          (top_sell.amount, top_sell.value))
+                    logger.info("Bought %s for $%s",
+                                top_sell.amount, top_sell.value)
                 elif top_buy.amount < top_sell.amount:
                     top_sell.amount -= top_buy.amount
                     self.sell_book.add(top_sell)
-                    print("Bought %s for $%s" %
-                          (top_buy.amount, top_sell.value))
+                    logger.info("Bought %s for $%s",
+                                top_buy.amount, top_sell.value)
                 else:
-                    print("Bought %s for $%s" %
-                          (top_buy.amount, top_sell.value))
+                    logger.info("Bought %s for $%s",
+                                top_buy.amount, top_sell.value)
